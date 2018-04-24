9ac53dc2805857f471804b12d6a927a0dddf51e9
galz10 <38544478+galz10@users.noreply.github.com>
2018-04-23 22:24:35 -0700

    game change 1
    added timer

--- CPTS122PA9/Header.h
+++ CPTS122PA9/Header.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <SFML\System.hpp>
+#include <math.h>
 #include <string>
 #include <iostream>
-#include "Header.h";
+
 
 
 using namespace std;
@@ -13,3 +15,22 @@ int run();
 void playerCollisions(CircleShape &rect, int type, int keyPress);
 void updatePlayer(CircleShape &rect, int keyPress);
 int setBackground(sf::RenderWindow & wind);
+void enemy(sf::RenderWindow &window);
+
+class Bullet
+{
+public:
+	CircleShape bullet;
+	Vector2f curvellocity;
+	float maxspeed;
+
+	Bullet(float radius = 5.f) :curvellocity(0.f,0.5f), maxspeed(15.f)
+	{
+		this->bullet.setRadius(radius);
+		this->bullet.setFillColor(Color::Red);
+	}
+
+
+private:
+
+};
--- CPTS122PA9/Source.cpp
+++ CPTS122PA9/Source.cpp
@@ -5,7 +5,21 @@ int run()
 	sf::CircleShape shape(10.f);
 	shape.setOrigin(sf::Vector2f(-395, -565));
 	shape.setFillColor(sf::Color::Green);
+
+	
+
+	sf::Font timerFont;
+	if (!timerFont.loadFromFile("font.ttf")) {
+		std::cerr << "No font file found!" << std::endl;
+	}
+	sf::Text timerText;
+	timerText.setFont(timerFont);
+	timerText.setPosition(400, 0);
+	timerText.setCharacterSize(40);
+
 	int i = 0;
+	sf::Clock clock;
+	sf::Time time;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -15,11 +29,21 @@ int run()
 				window.close();
 			if (event.KeyPressed && event.key.code == Keyboard::Escape)
 				window.close();
+
 		}
+
+		clock.restart();
+		time = clock.getElapsedTime();
+
+		enemy(window, shape);
+
+		timerText.setString(to_string(time.asSeconds()));
+		cout << time.asSeconds();
 		window.clear();
 		setBackground(window);
 		window.draw(shape);
 		updatePlayer(shape, i);
+		window.draw(timerText);
 		window.display();
 		
 	}
@@ -35,32 +59,21 @@ void playerCollisions(CircleShape & player, int type, int keyPress)
 	{
 	case 0: //A
 		///////////////////////////////////////////////////Outside structures//////////////////////////////////////////////////
-		if (player.getPosition().y >= -50 && (player.getPosition().x <= -185))
-		{
-			
-		}
-		else if (player.getPosition().y >= -35 && (player.getPosition().x <= -115))
-		{
-			
-		}
-		else if (player.getPosition().y >= -70 && (player.getPosition().x <= -250))
-		{
-			
-		}
-		else if (player.getPosition().x <= -60 && (player.getPosition().y <20 && player.getPosition().y >-20))
-		{
-			
-		}
-		else if (player.getPosition().x <= -140 && (player.getPosition().y <50 && player.getPosition().y >-85))
-		{
-
-		}
-		else if (player.getPosition().x <= -95 && (player.getPosition().y >-135 && player.getPosition().y <-85))
-		{
-
-		}
-		
+		if (player.getPosition().y >= -50 && (player.getPosition().x <= -185)) {}
 		///////////////////////////////////////////////////Inside structures//////////////////////////////////////////////////
+		else if (player.getPosition().y >= -35 && (player.getPosition().x <= -115)) {}
+		else if (player.getPosition().y >= -70 && (player.getPosition().x <= -250)) {}
+		else if (player.getPosition().x <= -60 && (player.getPosition().y <20 && player.getPosition().y >-20)) {}
+		else if (player.getPosition().x <= -140 && (player.getPosition().y <50 && player.getPosition().y >-85)) {}
+		else if (player.getPosition().x <= -95 && (player.getPosition().y > -135 && player.getPosition().y < -85)) {}
+		else if ((player.getPosition().x <= 70 && player.getPosition().x > -70) && player.getPosition().y == -90) {}
+		else if ((player.getPosition().x <= -165&& player.getPosition().x >=-259) && (player.getPosition().y > -245&& player.getPosition().y < -175)) {}
+		else if ((player.getPosition().x >= 0 && player.getPosition().x <= 20) && (player.getPosition().y <=-340 )) {}
+		else if ((player.getPosition().x <= 0 && player.getPosition().x >= -30) && (player.getPosition().y > -320 && player.getPosition().y < -275)) {}
+		else if ((player.getPosition().x <= -95 && player.getPosition().x >= -105) && (player.getPosition().y > -265 && player.getPosition().y < -235)) {}
+		else if ((player.getPosition().x <= -190 && player.getPosition().x >= -230) && (player.getPosition().y > -380 && player.getPosition().y < -280)) {}
+		else if ((player.getPosition().x <= -110 && player.getPosition().x >= -125) && (player.getPosition().y > -510 && player.getPosition().y < -400)) {}
+
 		else
 		{
 			player.move(-5.f, 0.f);
@@ -74,23 +87,18 @@ void playerCollisions(CircleShape & player, int type, int keyPress)
 		{
 
 		}
-		else if ((player.getPosition().y > -75) && (player.getPosition().x > -260 && (player.getPosition().x < -190)))
-		{
-
-		}
-		else if ((player.getPosition().y > -55) && (player.getPosition().x > -195 && (player.getPosition().x < -120)))
-		{
-
-		}
-		else if ((player.getPosition().y > -40) && (player.getPosition().x > -125 && (player.getPosition().x < -65)))
-		{
-
-		}
-		else if ((player.getPosition().y > -40) && (player.getPosition().x > 60 && (player.getPosition().x < 95)))
-		{
-
-		}
 		///////////////////////////////////////////////////Inside structures//////////////////////////////////////////////////
+		else if ((player.getPosition().y > -185 && player.getPosition().y < -70) && (player.getPosition().x > -67 && (player.getPosition().x < 65))) {}
+		else if (((player.getPosition().y > -150 && player.getPosition().y < -140) && (player.getPosition().x < 100 && player.getPosition().x>60))) {}
+		else if (((player.getPosition().y > -145 && player.getPosition().y < -135) && (player.getPosition().x >-195 && player.getPosition().x<-110))) {}
+		else if (((player.getPosition().y > -385 && player.getPosition().y < -375) && (player.getPosition().x >-260 && player.getPosition().x<-210))) {}
+		else if (((player.getPosition().y > -360 &&player.getPosition().y < -350)&& (player.getPosition().x >-125 && player.getPosition().x<-80))) {}
+		else if (((player.getPosition().y > -395 && player.getPosition().y <-380) &&(player.getPosition().x >-95 && player.getPosition().x<-65))) {}
+		else if (((player.getPosition().y > -250 && player.getPosition().y <-240) && (player.getPosition().x >-250 && player.getPosition().x<-185))) {}
+		else if (((player.getPosition().y > -325 && player.getPosition().y <-315) && (player.getPosition().x >-150 && player.getPosition().x<-120))) {}
+		else if (((player.getPosition().y > -505 && player.getPosition().y < -495)&&(player.getPosition().x >-201 && player.getPosition().x<-125))) {}
+		
+
 		else
 		{
 			player.move(0.f, 5.f);
@@ -102,13 +110,19 @@ void playerCollisions(CircleShape & player, int type, int keyPress)
 		///////////////////////////////////////////////////Outside structures//////////////////////////////////////////////////
 		if (player.getPosition().x >= 55 && (player.getPosition().y <20 && player.getPosition().y >-20))
 		{
-
+	
 		}
 		///////////////////////////////////////////////////Inside structures//////////////////////////////////////////////////
-		if (player.getPosition().x >= -70 && (player.getPosition().y <-80 && player.getPosition().y >-175))
-		{
+		else if (player.getPosition().x >= -70 && (player.getPosition().y <-70 && player.getPosition().y >-175)) {}
+		else if ((player.getPosition().x >= -260 && player.getPosition().x <= -245)&& (player.getPosition().y <-170 && player.getPosition().y >-250)) {}
+		else if ((player.getPosition().x >= -275 && player.getPosition().x <= -256) && (player.getPosition().y <-270 && player.getPosition().y >-390)) {}
+		else if ((player.getPosition().x >= -230 && player.getPosition().x <= -200) && (player.getPosition().y <-395 && player.getPosition().y >-510)) {}
+		else if ((player.getPosition().x >= -165 && player.getPosition().x <= -150) && (player.getPosition().y <-235 && player.getPosition().y >-320)) {}
+		else if ((player.getPosition().x >= -125 && player.getPosition().x <= -115) && (player.getPosition().y <-315 && player.getPosition().y >-360)) {}
+		else if ((player.getPosition().x >= -90 && player.getPosition().x <= -70) && (player.getPosition().y <-270 && player.getPosition().y >-390)) {}
+		else if ((player.getPosition().x >= -75 && player.getPosition().x <= -60) && (player.getPosition().y <-365 && player.getPosition().y >-560)) {}
+		else if ((player.getPosition().x >= -190 && player.getPosition().x <= -185) && (player.getPosition().y <-55 && player.getPosition().y >-160)) {}
 
-		}
 		else 
 		{
 			player.move(5.f, 0.f);
@@ -119,28 +133,18 @@ void playerCollisions(CircleShape & player, int type, int keyPress)
 	case 3: // w
 		///////////////////////////////////////////////////Outside structures//////////////////////////////////////////////////
 		///////////////////////////////////////////////////Inside structures//////////////////////////////////////////////////
-		if (player.getPosition().y <= -180 && (player.getPosition().x <55 && player.getPosition().x >-55))
-		{
-			player.move(0.f, -5.f);
-			cout << player.getPosition().x << player.getPosition().y << endl;
-			keyPress = 0;
-		}
-		else if ((player.getPosition().x > -145 && player.getPosition().x < -100)&& (player.getPosition().x > -80 && player.getPosition().x < -175))
-		{
-
-		}
-		else if (player.getPosition().y <= -70 && (player.getPosition().x > -55 && player.getPosition().x < 60))
-		{
-			
-		}
-		else if (player.getPosition().y <= -90 && (player.getPosition().x >55 && player.getPosition().x <100))
-		{
-			
-		}
-		else if ((player.getPosition().y <= -70 && player.getPosition().y >= -100 ) && (player.getPosition().x >-150 && player.getPosition().x <-100))
-		{
-
-		}
+		if ((player.getPosition().x > -145 && player.getPosition().x < -100) && (player.getPosition().x > -80 && player.getPosition().x < -175)) {}
+		else if ((player.getPosition().y <= -70 && player.getPosition().y > -180) && (player.getPosition().x > -55 && player.getPosition().x < 60)) {}
+		else if ((player.getPosition().y <= -90 && player.getPosition().y >= -145) && (player.getPosition().x > 55 && player.getPosition().x < 100)) {}
+		else if ((player.getPosition().y <= -70 && player.getPosition().y >= -100) && (player.getPosition().x > -150 && player.getPosition().x < -100)) {}
+		else if ((player.getPosition().y <= -230 && player.getPosition().y >= -318) && (player.getPosition().x > -165 && player.getPosition().x < -100)) {}
+		else if ((player.getPosition().y <= -245 && player.getPosition().y >= -260) && (player.getPosition().x > -105 && player.getPosition().x < -55)){}
+		else if ((player.getPosition().y <= -255 && player.getPosition().y >= -260) && (player.getPosition().x > -55 && player.getPosition().x < -10)) {}
+		else if ((player.getPosition().y <= -310 && player.getPosition().y >= -365) && (player.getPosition().x > -15 && player.getPosition().x <15)) {}
+		else if ((player.getPosition().y <= -340 && player.getPosition().y >= -395) && (player.getPosition().x > -5 && player.getPosition().x <100)) {}
+		else if ((player.getPosition().y <= -170 && player.getPosition().y >= -230) && (player.getPosition().x > -255 && player.getPosition().x <-170)) {}
+		else if ((player.getPosition().y <= -270 && player.getPosition().y >= -360) && (player.getPosition().x > -260 && player.getPosition().x <-190)) {}
+		else if ((player.getPosition().y <= -395 && player.getPosition().y >= -485) && (player.getPosition().x > -205 && player.getPosition().x <-110)) {}
 		else 
 		{
 			player.move(0.f, -5.f);
@@ -195,3 +199,25 @@ int setBackground(sf::RenderWindow & wind)
 	shape1.setTexture(Bkg);
 	wind.draw(shape1);
 }
+
+void enemy(sf::RenderWindow & window , CircleShape & shape)
+{
+	Bullet b1;
+
+	vector<Bullet>bull;
+	bull.push_back(Bullet(b1));
+
+	if(shape.getPosition().x == 0 && shape.getPosition().y == 0)
+	{
+		b1.bullet.setPosition(Vector2f(100, 100));
+		b1.curvellocity = b1.curvellocity;
+
+		bull.push_back(Bullet(b1));
+	}
+	for (size_t i = 0; i < bull.size; i++)
+	{
+		bull[i].bullet.move(bull[i].curvellocity);
+	}
+
+}
+
